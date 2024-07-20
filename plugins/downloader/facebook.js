f79a761a973d0b3e914c7af6b5aa1220d3f7e9b0
rizalzall <102241575+rizalzall@users.noreply.github.com>
2024-07-21 06:33:08 +0700

    Update facebook.js

--- plugins/downloader/facebook.js
+++ plugins/downloader/facebook.js
@@ -9,11 +9,12 @@ async: async (m, { func, mecha }) => {
 if (!m.text) return m.reply(`Masukkan URL!\n\nContoh: *${m.cmd} https://www.facebook.com/watch/?v=1393572814172251*`)
 if (!m.args[0].match(/(?:https?:\/\/(web\.|www\.|m\.)?(facebook|fb)\.(com|watch)\S+)?$/)) return m.reply(global.mess.error.url)
 if (m.args[0].includes('https://l.facebook.com/l.php?u=')) return m.reply(global.mess.error.url)
-m.reply(global.mess.wait)
+mecha.sendReact(m.chat, '🕒', m.key)
 await fg.fbdl(m.args[0]).then(async res => {
 let txt = res.title ? res.title : global.mess.ok
 await mecha.sendMedia(m.chat, res.videoUrl, m, { caption: txt, expiration: m.expiration });
 })
+  mecha.sendReact(m.chat, '☑️', m.key)
 },
 premium: false,
 limit: 2
