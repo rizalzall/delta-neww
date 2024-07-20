f4805b0c6fc9fad410ddc8517bae5ff5ffd5e3f5
rizalzall <102241575+rizalzall@users.noreply.github.com>
2024-07-21 06:25:02 +0700

    Update capcut.js

--- plugins/downloader/capcut.js
+++ plugins/downloader/capcut.js
@@ -6,7 +6,7 @@ exports.run = {
     async: async (m, { func, mecha, comand }) => {
         if (!m.text) return m.reply(func.example(m.cmd, 'https://www.capcut.com/'))
         if (!func.isUrl(m.args[0]) && !m.args[0].includes('capcut.com')) return m.reply(mess.error.url)
-        m.reply(global.mess.wait)
+        mecha.sendReact(m.chat, '🕒', m.key)
         let res = await func.fetchJson('https://aemt.me/download/capcut?url=' + m.text)
         if (!res.code === 0) return m.reply(mess.error.api)
         let {
@@ -19,6 +19,7 @@ exports.run = {
     } = res.result;
 let cap = `▶️ *C A P C U T* ▶️\n\n*Judul:* ${title}\n*Digunakan:* ${digunakan}\n*Deskripsi:* ${description}\n*Cover:* ${cover}\n*Profile:* ${author_profile}`;
         await mecha.sendMessage(m.chat, { video: { url: video_ori }, caption: cap, mimetype: 'video/mp4' }, { quoted: m, ephemeralExpiration: m.expiration })
+    mecha.sendReact(m.chat, '☑️', m.key)
     },
     limit: true
 }
