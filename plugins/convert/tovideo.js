f157d77b57c5de5da0826000d465f65e68e5c126
rizalzall <102241575+rizalzall@users.noreply.github.com>
2024-07-26 16:28:52 +0700

    Update tovideo.js

--- plugins/convert/tovideo.js
+++ plugins/convert/tovideo.js
@@ -1,64 +1,56 @@
 const fs = require('fs');
 const path = require('path');
 const { exec } = require('child_process');
-const { fromBuffer } = require('file-type');
 
-function toMedia(bufferIn, func, isAnimated) {
-return new Promise(async (resolve, reject) => {
-let extension = (await fromBuffer(bufferIn)).ext
-// if (!/webp/i.test(extension)) return reject("Hanya bisa webp")
-let png = path.join(process.cwd(), 'sampah', func.filename('png'))
-let webp = path.join(process.cwd(), 'sampah', func.filename('webp'))
-let mp4 = path.join(process.cwd(), 'sampah', func.filename('mp4'))
-let gif = path.join(process.cwd(), 'sampah', func.filename('gif'))
-await fs.writeFileSync(webp, bufferIn)
-if (isAnimated) {
-await exec(`convert ${webp} ${gif}`, async (err) => {
-if (err) {
-await fs.unlinkSync(webp)
-return reject(err);
-}
-await exec(`ffmpeg -i ${gif} -pix_fmt yuv420p -c:v libx264 -movflags +faststart -filter:v crop='floor(in_w/2)*2:floor(in_h/2)*2' ${mp4}`, async (err) => {
-if (err) {
-await fs.unlinkSync(webp)
-await fs.unlinkSync(gif)
-return reject(err);
-}
-let buff = await fs.readFileSync(mp4);
-await fs.unlinkSync(webp);
-await fs.unlinkSync(gif);
-await fs.unlinkSync(mp4);
-resolve({ buffer: buff, isVideo: true });
-});
-});
-} else if (!isAnimated) {
-await exec(`ffmpeg -i ${webp} ${png}`, async (err) => {
-if (err) {
-await fs.unlinkSync(webp)
-return reject(err)
-}
-let buff = await fs.readFileSync(png);
-await fs.unlinkSync(webp);
-await fs.unlinkSync(png);
-resolve({ buffer: buff, isVideo: false });
-});
-}
-})
+function webpToVideo(bufferImage) {
+  return new Promise((resolve, reject) => {
+    try {
+      let pathFile = "./sampah/" + ~~(Math.random() * 1000000 + 1) + ".webp";
+      fs.writeFileSync(pathFile, bufferImage);
+      exec(`convert ${pathFile} ${pathFile}.gif`, (error) => {
+        if (error) return reject(error);
+        exec(`ffmpeg -i ${pathFile}.gif -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" ${pathFile}.mp4`, (error) => {
+          if (error) return reject(error);
+          if (!fs.existsSync(pathFile + ".gif") || !fs.existsSync(pathFile + ".mp4")) {
+            reject("Failed convert file!");
+            fs.unlinkSync(pathFile);
+            return;
+          }
+          let videoBuffer = fs.readFileSync(pathFile + ".mp4");
+          fs.unlinkSync(pathFile);
+          fs.unlinkSync(pathFile + ".gif");
+          fs.unlinkSync(pathFile + ".mp4");
+          resolve(videoBuffer);
+        });
+      });
+    } catch (e) {
+      reject(e);
+    }
+  });
 }
 
 exports.run = {
-usage: ['tovideo'],
-hidden: ['tomp4'],
-use: 'reply sticker',
-category: 'convert',
-async: async (m, { func, mecha, quoted }) => {
-if (!global.support.convert && !global.support.magick && !global.support.gm) return m.reply('This feature does not work because imagemagick is not installed.')
-if (/webp/.test(quoted.mime)){
-m.reply(global.mess.wait)
-let media = await quoted.download()
-let conv = await toMedia(media, func, quoted.isAnimated);
-await mecha.sendMessage(m.chat, {video: conv.buffer, caption: 'Convert Webp To Video'}, {quoted: m, ephemeralExpiration: m.expiration})
-} else m.reply(`Reply sticker nya dengan caption ${m.cmd}`)
-},
-limit: true
-}
+  usage: ['tovideo'],
+  hidden: ['tomp4'],
+  use: 'reply sticker',
+  category: 'convert',
+  async: async (m, { func, mecha, quoted }) => {
+    try {
+      if (!global.support.convert && !global.support.magick && !global.support.gm) {
+        return m.reply('This feature does not work because imagemagick is not installed.');
+      }
+      if (/webp/.test(quoted.mime)) {
+        m.reply(global.mess.wait);
+        let media = await quoted.download();
+        let videoBuffer = await webpToVideo(media);
+        await mecha.sendMessage(m.chat, { video: videoBuffer, caption: 'Convert Webp To Video' }, { quoted: m, ephemeralExpiration: m.expiration });
+      } else {
+        m.reply(`Reply sticker nya dengan caption ${m.cmd}`);
+      }
+    } catch (error) {
+      console.error(error);
+      m.reply(`Terjadi Kesalahan: ${error}`);
+    }
+  },
+  limit: 5
+};
