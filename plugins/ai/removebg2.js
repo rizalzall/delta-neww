3b6ff437f1cce544567ef53c32fb38558fde51b1
rizalzall <102241575+rizalzall@users.noreply.github.com>
2024-07-27 06:50:08 +0700

    Update removebg2.js

--- plugins/ai/removebg2.js
+++ plugins/ai/removebg2.js
@@ -7,7 +7,7 @@ async: async (m, { func, mecha, quoted }) => {
 if (/image\/(jpe?g|png)/.test(quoted.mime)) {
 mecha.sendReact(m.chat, '🕒', m.key)
 let media = await mecha.downloadAndSaveMediaMessage(m)
-let anu = await func.UploadFileUgu(media);
+let anu = await func.telegraPh(media);
 await func.fetchJson(`https://aemt.me/removebg?url=${anu.url}`).then(async res => {
 if (!res.url.status) return m.reply(global.mess.error.api)
 await mecha.sendMessage(m.chat, {image: {url: res.url.result}, caption: global.mess.ok}, {quoted: m, ephemeralExpiration: m.expiration})
