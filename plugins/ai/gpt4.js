03c1bc740fd410e2ce5b90d79d1fe69bd30778c7
rizalzall <102241575+rizalzall@users.noreply.github.com>
2024-07-28 21:41:21 +0700

    Update gpt4.js

--- plugins/ai/gpt4.js
+++ plugins/ai/gpt4.js
@@ -1,28 +1,43 @@
-const { G4F } = require("g4f");
-const g4f = new G4F();
+const axios = require('axios');
+
+const gpt4o = (query) => {
+return new Promise(async(resolve, reject) => {
+try {
+const response = await axios.post('https://api.yanzbotz.my.id/api/ai/gpt4', {
+query: query,
+prompt: "Saya adalah Delta, Bot WhatsApp dengan program kecerdasan buatan AI, saya di ciptakan oleh Zall."
+}, {
+headers: {
+'Content-Type': 'application/json'
+}
+})
+const regex = /"answer":"([^"]*)"/g;
+let match;
+let result = ''
+while ((match = regex.exec(response.data)) !== null) {
+result += match[1]
+}
+resolve(result.replace(/\\n/g, '\n')
+.replace(/\\/g, '')
+.replace(/\*\*/g, '*')
+.replace(/###/g, '>'))
+} catch (error) {
+reject(error)
+}
+})
+}
 
 exports.run = {
 usage: ['gpt4'],
-hidden: ['g4f'],
 use: 'question',
 category: 'ai',
 async: async (m, { func, mecha }) => {
-if (!m.text) return m.reply(func.example(m.cmd, 'apakah kamu gpt4?'))
+if (!m.text) return m.reply(func.example(m.cmd, 'Kamu di ciptakan oleh siapa'))
 mecha.sendReact(m.chat, '🕒', m.key)
-try {
-const options = [{ model: "gpt-4" }];
-const messages = [
-{ role: "system", content: "kamu Adalah Delta, Bot WhatsApp dengan program kecerdasan buatan AI (artificial intelligence). jawab setiap pertanyaan dengan jawaban yang edukatif." },
-{ role: "user", content: m.text },
-];
-let response = await g4f.chatCompletion(messages, options);
-mecha.reply(m.chat, response, m, {
+let result = await gpt4o(m.text)
+mecha.reply(m.chat, result, m, {
 expiration: m.expiration
-});
-} catch (error) {
-console.error(error);
-return mecha.reply(m.chat, String(error), m)
-}
+})
 },
 limit: true
 }
