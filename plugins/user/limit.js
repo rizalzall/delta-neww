a1662c3f4c73ff3acb6f5ce5126a0fd699d06c9b
rizalzall <102241575+rizalzall@users.noreply.github.com>
2024-08-05 12:56:07 +0700

    Update limit.js

--- plugins/user/limit.js
+++ plugins/user/limit.js
@@ -1,37 +1,3 @@
-/*exports.run = {
-usage: ['limit'],
-hidden: ['ceklimit', 'balance'],
-use: 'mention or reply',
-category: 'user',
-async: async (m, { func, mecha, setting, froms, isPrem }) => {
-let txt;
-if (m.quoted || m.text) {
-if (global.db.users[froms] == undefined) return m.reply('User data not found.')
-let isPrim = global.db.users[froms].premium
-txt = `Limit ${global.db.users[froms].name}
-
-Status: ${isPrim ? 'Premium' : 'Gratisan'}
-Limit: ${global.db.users[froms].limit}/${isPrim ? 99999 : setting.limit}
-Balance: $${func.rupiah(global.db.users[froms].balance)}
-
-> untuk membeli *LIMIT* ketik ${m.prefix}buylimit
-> untuk membeli *UNLIMITED* ketik ${m.prefix}buyprem
-> untuk membeli *BALANCE* ketik ${m.prefix}buybalance`
-} else {
-txt = `${m.pushname}
-
-Status: ${m.isOwner ? 'Owner' : isPrem ? 'Premium' : 'Gratisan'}
-Limit: ${global.db.users[m.sender].limit}/${isPrem ? 99999 : setting.limit}
-Balance: $${func.rupiah(global.db.users[m.sender].balance)}
-
-> untuk membeli *LIMIT* ketik ${m.prefix}buylimit
-> untuk membeli *UNLIMITED* ketik ${m.prefix}buyprem
-> untuk membeli *BALANCE* ketik ${m.prefix}buybalance`
-}
-mecha.reply(m.chat, txt, m)
-}
-}*/
-
 const fetch = require('node-fetch');
 
 exports.run = {
@@ -52,8 +18,8 @@ thumbnail: await (await fetch(await mecha.profilePictureUrl(froms, 'image').catc
 url: setting.link, 
 expiration: m.expiration
 })
-} else mecha.sendMessageModify(m.chat, `Limit ${m.pushname}`, m, {
-title: `Status: ${m.isOwner ? 'Owner' : isPrem ? 'Premium' : 'Gratisan'}\nLimit: ${global.db.users[m.sender].limit}/${isPrem ? 99999 : setting.limit}`,
+} else mecha.sendMessageModify(m.chat, `• Limit ${m.pushname}`, m, {
+title: `Status: ${m.isOwner ? 'Owner' : isPrem ? 'Premium' : 'Gratisan'}\n• Limit: ${global.db.users[m.sender].limit}/${isPrem ? 99999 : setting.limit}`,
 body: `Balance: $${func.rupiah(global.db.users[m.sender].balance)}`,
 thumbnail: await (await fetch(await mecha.profilePictureUrl(m.sender, 'image').catch(_ => setting.cover))).buffer(),
 url: setting.link, 
